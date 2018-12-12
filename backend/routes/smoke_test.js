1a087c188cd705c26733f8fa8354df4c09f2e7d7
kjsuarez <kjsuarez94@gmail.com>
2018-12-11 20:28:25 -0800

    successfully get historical data from polygon api

--- backend/routes/smoke_test.js
+++ backend/routes/smoke_test.js
@@ -1,12 +1,54 @@
 var express = require('express');
 var router = express.Router();
 var jwt = require('jsonwebtoken');
+const Alpaca = require('@alpacahq/alpaca-trade-api')
+var request = require('request-promise');
+//
 
 router.get('/', function (req, res, next) {
-  res.status(200).json({
-    message: 'success',
-    obj: ["this is a testestestest"]
+
+  const alpaca = new Alpaca({
+    keyId: process.env.ALPACA_PAPER_KEY,
+    secretKey: process.env.ALPACA_PAPER_SECRET,
+    paper: true
+  })
+
+  alpaca.getAccount().then((account) => {
+    console.log('Current Account:', account)
+
+    res.status(200).json({
+      message: 'success',
+      obj: account
+    });
+  })
+
+
+});
+
+router.get('/polygon', function (req, res, next) {
+
+  var options = {
+    uri: 'https://api.polygon.io/v1/historic/agg/day/AAPL',
+    qs: {
+        from: '4-1-2017',
+        to: '4-1-2018',
+        apiKey: process.env.ALPACA_PAPER_KEY
+    },
+    json: true // Automatically parses the JSON string in the response
+};
+
+request(options)
+  .then((result) => {
+    console.log('result:', result)
+    res.status(200).json({
+      message: 'success',
+      obj: result
+    });
+  })
+  .catch(function (err) {
+      // API call failed...
   });
+
 });
 
 module.exports = router;
